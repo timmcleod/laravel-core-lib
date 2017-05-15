1500cce178de0788667881a2ae4392a74db4a266
Tim McLeod <tim@timmcleod.com>
2017-05-15 16:55:48 -0400

    Added more tests for the ChangeTrackable trait

--- tests/ChangeTrackableTest.php
+++ tests/ChangeTrackableTest.php
@@ -128,7 +128,7 @@ class ChangeTrackableTest extends TestCase
         ], $this->user->getTrackedChangesArray());
     }
 
-    public function testAgeTrackedGetTrackedChangesArray()
+    public function testGetTrackedChangesArrayInAgeTrackedModel()
     {
         // Change user's name
         $this->user2->name = 'Calvin Klein';
@@ -151,8 +151,432 @@ class ChangeTrackableTest extends TestCase
 
         // Age has changed after a save event, so expect to see those changes
         $this->assertEquals([
-            'age'  => ['old' => 50, 'new' => 51],
+            'age' => ['old' => 50, 'new' => 51],
         ], $this->user2->getTrackedChangesArray());
+    }
+
+    public function testGetTrackedChangesArrayFor()
+    {
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user->getTrackedChangesArrayFor(['name']));
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user->getTrackedChangesArrayFor([]));
+
+        // Expect to see no changes returned when asking for changes for 'age' attribute
+        $this->assertEquals([], $this->user->getTrackedChangesArrayFor(['age']));
+
+        // Change user's age
+        $this->user->age = 51;
+        $this->user->save();
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'age'  => ['old' => 50, 'new' => 51],
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user->getTrackedChangesArrayFor([]));
+
+        // Expect to see some changes returned when asking for changes for 'age' attribute
+        $this->assertEquals([
+            'age' => ['old' => 50, 'new' => 51]
+        ], $this->user->getTrackedChangesArrayFor(['age']));
+    }
+
+    public function testGetTrackedChangesArrayForInAgeTrackedModel()
+    {
+        // The getTrackedChangesArrayFor() method should behave exactly the same
+        // regardless of whether or not the $trackable property is defined, so
+        // these assertions are exactly the same as the ones in the last test
+        // except that these are using $user2, which has $trackable defined
+
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+        $this->user2->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user2->getTrackedChangesArrayFor(['name']));
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user2->getTrackedChangesArrayFor([]));
+
+        // Expect to see no changes returned when asking for changes for 'age' attribute
+        $this->assertEquals([], $this->user2->getTrackedChangesArrayFor(['age']));
+
+        // Change user's age
+        $this->user2->age = 51;
+        $this->user2->save();
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'age'  => ['old' => 50, 'new' => 51],
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user2->getTrackedChangesArrayFor([]));
+
+        // Expect to see some changes returned when asking for changes for 'age' attribute
+        $this->assertEquals([
+            'age' => ['old' => 50, 'new' => 51]
+        ], $this->user2->getTrackedChangesArrayFor(['age']));
+    }
+
+    public function testGetTrackedChangesArrayForAll()
+    {
+        // The getTrackedChangesArrayForAll() method should always return
+        // all of the changes that have been tracked, regardless of
+        // whether or not $trackable is defined on the model.
+
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user->getTrackedChangesArrayForAll());
+
+        // Change user's age
+        $this->user->age = 51;
+        $this->user->save();
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'age'  => ['old' => 50, 'new' => 51],
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user->getTrackedChangesArrayForAll());
+    }
+
+    public function testGetTrackedChangesArrayForAllInAgeTrackedModel()
+    {
+        // The getTrackedChangesArrayForAll() method should always return
+        // all of the changes that have been tracked, regardless of
+        // whether or not $trackable is defined on the model.
+
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+        $this->user2->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals([
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user2->getTrackedChangesArrayForAll());
+
+        // Change user's age
+        $this->user2->age = 51;
+        $this->user2->save();
+
+        // Expect to see all changes when passing empty array for attributes param
+        $this->assertEquals([
+            'age'  => ['old' => 50, 'new' => 51],
+            'name' => ['old' => 'Marty', 'new' => 'Calvin Klein']
+        ], $this->user2->getTrackedChangesArrayForAll());
+    }
+
+    public function testHasTrackedChanges()
+    {
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals(false, $this->user->hasTrackedChanges());
+
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user->hasTrackedChanges());
+
+        // Change user's age
+        $this->user->age = 51;
+        $this->user->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user->hasTrackedChanges());
+    }
+
+    public function testHasTrackedChangesInAgeTrackedModel()
+    {
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+        $this->user2->save();
+
+        // We shouldn't see any changes since `name` isn't marked as trackable.
+        $this->assertEquals(false, $this->user2->hasTrackedChanges());
+
+        // Change user's age
+        $this->user2->age = 51;
+        $this->user2->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user2->hasTrackedChanges());
+    }
+
+    public function testHasAnyTrackedChanges()
+    {
+        // No changes have been made yet, so nothing should be tracked
+        $this->assertEquals(false, $this->user->hasAnyTrackedChanges());
+
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals(false, $this->user->hasAnyTrackedChanges());
+
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user->hasAnyTrackedChanges());
+
+        // Change user's age
+        $this->user->age = 51;
+        $this->user->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user->hasAnyTrackedChanges());
+    }
+
+    public function testHasAnyTrackedChangesInAgeTrackedModel()
+    {
+        // No changes have been made yet, so nothing should be tracked
+        $this->assertEquals(false, $this->user2->hasAnyTrackedChanges());
+
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+        $this->user2->save();
+
+        // We should see changes since this method doesn't consider what is $trackable
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChanges());
+
+        // Change user's age
+        $this->user2->age = 51;
+        $this->user2->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChanges());
+    }
+
+    public function testHasAnyTrackedChangesFor()
+    {
+        // No changes have been made yet, so nothing should be tracked
+        $this->assertEquals(false, $this->user->hasAnyTrackedChangesFor(['name']));
+
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user->hasAnyTrackedChangesFor(['name']));
+
+        // Expect to see NO changes when passing empty array for attributes param
+        $this->assertEquals(false, $this->user->hasAnyTrackedChangesFor([]));
+
+        // Expect to see no changes returned when asking for changes for 'age' attribute
+        $this->assertEquals(false, $this->user->hasAnyTrackedChangesFor(['age']));
+
+        // Expect to see changes for either name or age since name has changed
+        $this->assertEquals(true, $this->user->hasAnyTrackedChangesFor(['name', 'age']));
+
+        // Change user's age
+        $this->user->age = 51;
+        $this->user->save();
+
+        // Expect to see changes for age after save
+        $this->assertEquals(true, $this->user->hasAnyTrackedChangesFor(['age']));
+
+        // Expect to see changes for either name or age
+        $this->assertEquals(true, $this->user->hasAnyTrackedChangesFor(['name', 'age']));
+
+        // Expect to see NO changes when passing empty array for attributes param
+        $this->assertEquals(false, $this->user->hasAnyTrackedChangesFor([]));
+    }
+    
+    public function testHasAnyTrackedChangesForInAgeTrackedModel()
+    {
+        // The hasAnyTrackedChangesFor() method should behave exactly the same
+        // regardless of whether or not the $trackable property is defined, so
+        // these assertions are exactly the same as the ones in the last test
+        // except that these are using $user2, which has $trackable defined
+
+        // No changes have been made yet, so nothing should be tracked
+        $this->assertEquals(false, $this->user2->hasAnyTrackedChangesFor(['name']));
+
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+        $this->user2->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChangesFor(['name']));
+
+        // Expect to see NO changes when passing empty array for attributes param
+        $this->assertEquals(false, $this->user2->hasAnyTrackedChangesFor([]));
+
+        // Expect to see NO changes returned when asking for changes for 'age' attribute
+        $this->assertEquals(false, $this->user2->hasAnyTrackedChangesFor(['age']));
+
+        // Expect to see changes for either name or age since name has changed
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChangesFor(['name', 'age']));
+
+        // Change user's age
+        $this->user2->age = 51;
+        $this->user2->save();
+
+        // Expect to see changes for age after save
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChangesFor(['age']));
+
+        // Expect to see changes for either name or age
+        $this->assertEquals(true, $this->user2->hasAnyTrackedChangesFor(['name', 'age']));
+
+        // Expect to see NO changes when passing empty array for attributes param
+        $this->assertEquals(false, $this->user2->hasAnyTrackedChangesFor([]));
+    }
+
+    public function testGetTrackedChanges()
+    {
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals('', $this->user->getTrackedChanges());
+
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user->getTrackedChanges());
+
+        // Change the format of those changes:
+        $this->assertEquals('name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChanges('{attribute} was changed from "{old}" to "{new}"'));
+
+        // Change user's age
+        $this->user->age = 51;
+
+        // Save hasn't happened yet, so expect no new tracked changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user->getTrackedChanges());
+
+        $this->user->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals('age: 50 > 51 | name: Marty > Calvin Klein', $this->user->getTrackedChanges());
+
+        // Change the format and delimiter of those changes:
+        $this->assertEquals(
+            'age was changed from "50" to "51"' . PHP_EOL .
+            'name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChanges('{attribute} was changed from "{old}" to "{new}"', PHP_EOL));
+
+        // Change the ID
+        $this->user->id = 1000;
+        $this->user->save();
+
+        $this->assertEquals(
+            'age was changed from "50" to "51"' . PHP_EOL .
+            'id was changed from "1" to "1000"' . PHP_EOL .
+            'name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChanges('{attribute} was changed from "{old}" to "{new}"', PHP_EOL));
+
+        // Change the age to null
+        $this->user->age = null;
+        $this->user->save();
+
+        $this->assertEquals(
+            'age was changed from "50" to "zilch"' . PHP_EOL .
+            'id was changed from "1" to "1000"' . PHP_EOL .
+            'name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChanges('{attribute} was changed from "{old}" to "{new}"', PHP_EOL, 'nada', 'zilch'));
+    }
+
+    public function testGetTrackedChangesInAgeTrackedModel()
+    {
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals('', $this->user2->getTrackedChanges());
+
+        $this->user2->save();
+
+        // Name is not in trackable array, so expect NO new tracked changes
+        $this->assertEquals('', $this->user2->getTrackedChanges());
+
+        // Change user's age
+        $this->user2->age = 51;
+
+        // Save hasn't happened yet, so expect no new tracked changes
+        $this->assertEquals('', $this->user2->getTrackedChanges());
+
+        $this->user2->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals('age: 50 > 51', $this->user2->getTrackedChanges());
+    }
+
+    public function testGetTrackedChangesForAll()
+    {
+        // Change user's name
+        $this->user->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals('', $this->user->getTrackedChangesForAll());
+
+        $this->user->save();
+
+        // Name has changed after a save event, so expect to see those changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user->getTrackedChangesForAll());
+
+        // Change the format of those changes:
+        $this->assertEquals('name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChangesForAll('{attribute} was changed from "{old}" to "{new}"'));
+
+        // Change user's age
+        $this->user->age = 51;
+
+        // Save hasn't happened yet, so expect no new tracked changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user->getTrackedChangesForAll());
+
+        $this->user->save();
+
+        // Age has changed after a save event, so expect to see those changes too
+        $this->assertEquals('age: 50 > 51 | name: Marty > Calvin Klein', $this->user->getTrackedChangesForAll());
+
+        // Change the format and delimiter of those changes:
+        $this->assertEquals(
+            'age was changed from "50" to "51"' . PHP_EOL .
+            'name was changed from "Marty" to "Calvin Klein"',
+            $this->user->getTrackedChangesForAll('{attribute} was changed from "{old}" to "{new}"', PHP_EOL));
+    }
+
+    public function testGetTrackedChangesForAllInAgeTrackedModel()
+    {
+        // Change user's name
+        $this->user2->name = 'Calvin Klein';
+
+        // Save hasn't happened yet, so expect no tracked changes
+        $this->assertEquals('', $this->user2->getTrackedChangesForAll());
+
+        $this->user2->save();
+
+        // Name is not in trackable array, but we should still see the changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user2->getTrackedChangesForAll());
+
+        // Change user's age
+        $this->user2->age = 51;
+
+        // Save hasn't happened yet, so expect no new tracked changes
+        $this->assertEquals('name: Marty > Calvin Klein', $this->user2->getTrackedChangesForAll());
+
+        $this->user2->save();
+
+        // Age has changed after a save event, so expect to see those changes
+        $this->assertEquals('age: 50 > 51 | name: Marty > Calvin Klein', $this->user2->getTrackedChangesForAll());
     }
 
     /**
