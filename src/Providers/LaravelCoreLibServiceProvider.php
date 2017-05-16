9e66694551f343942fa5d76280123ae36c149aed
Tim McLeod <tim@timmcleod.com>
2017-05-16 16:09:35 -0400

    Added ViewModel generator artisan command

---
+++ src/Providers/LaravelCoreLibServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TimMcLeod\LaravelCoreLib\Providers;
+
+use Illuminate\Foundation\Providers\ArtisanServiceProvider;
+use TimMcLeod\LaravelCoreLib\Generators\ViewModelMakeCommand;
+
+class LaravelCoreLibServiceProvider extends ArtisanServiceProvider
+{
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [];
+
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $devCommands = [
+        'ViewModelMake' => 'command.view-model.make',
+    ];
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerViewModelMakeCommand()
+    {
+        $this->app->singleton('command.view-model.make', function ($app)
+        {
+            return new ViewModelMakeCommand($app['files']);
+        });
+    }
+}
